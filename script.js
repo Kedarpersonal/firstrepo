ca14e8368555ba7ea5e36fddbe6a60621d49c3e2
unknown <kedarpatil234@gmail.com>
2025-02-07 19:35:26 +0530

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
+    "Are you sure Mazi jaanudi?",
+    "Really sure Mera Baccha ??",
+    "Are you positive Meri jaan?",
+    "Pookie please naa...",
     "Just think about it!",
-    "If you say no, I will be really sad...",
+    "Tumhi Nahi mhananr, I will be really sad...",
     "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "I will be very very very sad bubu...",
+    "say yes please! meri jaaanu ❤️"
 ];
 
 let messageIndex = 0;
